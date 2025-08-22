6185689488ac831e90c34161b6a90c694252270f
jheloni <fuentesjheloni@gmail.com>
2025-08-22 16:56:37 +0800

    color was change

--- app/goals/create.jsx
+++ app/goals/create.jsx
@@ -47,7 +47,7 @@ const styles = StyleSheet.create({
   },
   button: {
     padding: 18,
-    backgroundColor: '#21cc8d',
+    backgroundColor: 'skyblue',
     color: 'white',
     borderRadius: 8,
   }
